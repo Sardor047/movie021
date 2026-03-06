1e821740d079e83768a12c07bad21b00d1a9e1bb
Sardor047 <zohidov219@gmail.com>
2026-03-06 12:27:52 +0500

    movie

--- kino/detail.js
+++ kino/detail.js
@@ -4,19 +4,19 @@ const path = new URLSearchParams(window.location.search);
 const id = path.get("id");
 let copyMovies = films;
 
-
 copyMovies.forEach((film) => {
   if (film.id === id) {
     let vaqt = new Date(film.release_date * 1000);
-    
+
     const kun = String(vaqt.getDate()).padStart(2, "0");
     const oy = String(vaqt.getMonth() + 1).padStart(2, "0");
     const yil = vaqt.getFullYear();
-    
+
     const release_date = `${kun}.${oy}.${yil}`;
     ul.innerHTML += `
      <div class="hero__wrapper">
      <div class="hero__left">
+     <a href="./index.html" class="back">return</a>
     <img class = "poster" src="${film.poster}" alt="">
     </div>
      <div class="hero__right">
@@ -29,4 +29,3 @@ copyMovies.forEach((film) => {
     `;
   }
 });
-
